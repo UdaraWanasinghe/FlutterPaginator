472d1fa56d3ef87232c3bc3c291e9148c7ba5ad0
UdaraWanasinghe <udara.developer@gmail.com>
2019-02-17 02:20:44 +0530

    Initial commit

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,10 +3,10 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_paginator_example/flutter_paginator/enums.dart';
 import 'package:http/http.dart' as http;
 
-import 'flutter_paginator/flutter_paginator.dart';
+import 'package:flutter_paginator/flutter_paginator.dart';
+import 'package:flutter_paginator/enums.dart';
 
 void main() => runApp(MyApp());
 
